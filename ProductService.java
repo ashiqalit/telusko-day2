79228c3a001ea1d3a4e6858dbfccacc13a46923b
ashiqalit <110171632+ashiqalit@users.noreply.github.com>
2023-05-22 23:19:15 +0530

    Add files via upload
    Implementation of,
    1.Search by place
    2.Search products out of warranty

---
+++ ProductService.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductService {
+
+    List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p){
+        products.add(p);
+    }
+
+    public List<Product> getAllProducts(){
+        return products;
+    }
+
+    public Product getProduct(String name){
+        for(Product p : products){
+            if(p.getName().equals(name))
+                return p;
+        }
+
+        return null;
+    }
+
+    public List<Product> getProductWithText(String text) {
+        String str = text.toLowerCase();
+        List<Product> prods = new ArrayList<>();
+
+        for(Product p : products){
+            String name = p.getName().toLowerCase();
+            String type = p.getType().toLowerCase();
+            String place = p.getPlace().toLowerCase();
+            if(name.contains(str) || type.contains(str) || place.contains(str))
+                prods.add(p);
+        }
+        return prods;
+
+    }
+
+	public List<Product> getProductByPlace(String place) {
+		List<Product> prods = new ArrayList<>();
+		for(Product p : products) {
+			if(p.getPlace().equalsIgnoreCase(place)) {
+				prods.add(p);
+			}
+		}
+		// TODO Auto-generated method stub
+		return prods;	
+	}
+	public List<Product> getOutOfWarrantyProducts() {
+		List<Product> prods = new ArrayList<>();
+		for(Product p : products) {
+			if(p.getWarranty() <= java.time.Year.now().getValue()) {
+				prods.add(p);
+			}
+		}
+		// TODO Auto-generated method stub
+		return prods;
+}
+}
